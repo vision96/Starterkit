474dde90269b095793efa32ee5ee222f1d148cc5
nida <98032052+vision96@users.noreply.github.com>
2022-09-06 12:19:28 +0300

    Create Cheque table and model

---
+++ database/migrations/2022_09_06_091351_create_cheques_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cheques', function (Blueprint $table) {
+            $table->id();
+            $table->string('cheque_number')->unique();
+            $table->BigInteger('bank_id')->unsigned();
+            $table->foreign('bank_id')->references('id')->on('banks')->onDelete('cascade');
+            $table->date('exchange_date')->nullable();
+            $table->string('cheque_recipient')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cheques');
+    }
+};
