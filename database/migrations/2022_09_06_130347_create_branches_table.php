5596d1a7da328352dcd2bdc8430e362572c30fc4
nida <98032052+vision96@users.noreply.github.com>
2022-09-06 17:13:22 +0300

    create branch table and model

---
+++ database/migrations/2022_09_06_130347_create_branches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('branch_name')->nullable();
+            $table->string('branch_number')->nullable();
+            $table->BigInteger('bank_id')->unsigned();
+            $table->foreign('bank_id')->references('id')->on('banks')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+};
