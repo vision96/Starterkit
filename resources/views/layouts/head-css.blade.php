bc8a492119a4e6a4101a9a6f368c58ca15929a29
nida <98032052+vision96@users.noreply.github.com>
2022-09-06 13:50:39 +0300

    Fix Master routes

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -1,11 +1,11 @@
 @yield('css')
 
 <!-- Bootstrap Css -->
-<link href="{{asset('assets/css/bootstrap.rtl.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('/assets/css/bootstrap.rtl.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="{{asset('assets/css/app.rtl.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('/assets/css/app.rtl.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
 
 
