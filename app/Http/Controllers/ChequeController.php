d3638701e9aa13ba895a5ec00e71c3904ad3679b
nida <98032052+vision96@users.noreply.github.com>
2022-09-07 14:08:27 +0300

    Add cheque blade and controller

--- app/Http/Controllers/ChequeController.php
+++ app/Http/Controllers/ChequeController.php
@@ -2,8 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bank;
+use App\Models\Cheque;
+use Illuminate\Http\Request;
+use DataTables;
 
 class ChequeController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
 
+            $data = Cheque::with('bank')->select('cheques.*');
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->editColumn('bank', function($data)
+                {
+                    return $data->bank->bank_name;
+                })
+                ->make(true);
+
+        }
+        return view('view-cheques');
+    }
+    /**
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $banks = Bank::all();
+        return view('add-cheque',compact('banks'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'cheque_number' => 'required|max:100',
+                'bank_id' => 'required|max:100',
+                'exchange_date' => 'required',
+                'cheque_recipient' => 'required|max:100',
+                'amount' => 'required',
+                'status' => 'required'
+            ]);
+
+            Cheque::create([
+                'cheque_number' => $request->cheque_number,
+                'bank_id' => $request->bank_id,
+                'exchange_date' => $request->exchange_date,
+                'cheque_recipient' => $request->cheque_recipient,
+                'amount' => $request->amount,
+                'status' => $request->status,
+            ]);
+
+            return response()->json(['success' => 'تمت الاضافة بنجاح']);
+        } catch (\exception $ex) {
+            return response()->json(['error' => 'هناك خطا ما ,حاول لاحقا', 'err' => $ex]);
+        }
+    }
 }
